b8ed912f7ee3d6c494f03c5ca3f59bb2f7c8e6a3
Rodrigo Grimaldo <74768218@certus.edu.pe>
2025-05-09 09:04:52 -0500

    agregar url

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const swaggerOption = {
         },
         servers: [
             {
+                url: 'https://certus2-production-9160.up.railway.app/'
+            },
+            {
                 url: 'http://localhost:4000'
             }
         ]
